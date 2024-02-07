c8d5d46c4cfacd840481697774d43c57c67b2877
rmamache <=>
2024-02-07 17:22:44 +0100

    test pour faire fonctionner l'insertion de fichiers

--- cra/host/src/FileUpload.js
+++ cra/host/src/FileUpload.js
@@ -1,52 +1,40 @@
-import React ,{useState} from 'react';
-import AWS from 'aws-sdk'
+import React , {useState} from 'react';
+import { uploadFile } from 'react-s3';
+import {Buffer} from 'buffer';
+Buffer.from('anything','base64');
+window.Buffer = window.Buffer || require("buffer").Buffer;
+
 
 const S3_BUCKET ='microcontainer';
 const REGION ='SBG';
+const ACCESS_KEY ='893452ce6d0a4c53b6932efca9ccd08e';
+const SECRET_ACCESS_KEY ='6733517fd8e84e8d98579efff530a14f';
 
-
-AWS.config.update({
-    accessKeyId: '893452ce6d0a4c53b6932efca9ccd08e',
-    secretAccessKey: '6733517fd8e84e8d98579efff530a14f'
-})
-
-const myBucket = new AWS.S3({
-    params: { Bucket: S3_BUCKET},
+const config = {
+    bucketName: S3_BUCKET,
     region: REGION,
-})
+    accessKeyId: ACCESS_KEY,
+    secretAccessKey: SECRET_ACCESS_KEY,
+}
 
 const FileUpload = () => {
 
-    const [progress , setProgress] = useState(0);
     const [selectedFile, setSelectedFile] = useState(null);
 
     const handleFileInput = (e) => {
         setSelectedFile(e.target.files[0]);
     }
 
-    const uploadFile = (file) => {
-
-        const params = {
-            ACL: 'public-read',
-            Body: file,
-            Bucket: S3_BUCKET,
-            Key: file.name
-        };
-
-        myBucket.putObject(params)
-            .on('httpUploadProgress', (evt) => {
-                setProgress(Math.round((evt.loaded / evt.total) * 100))
-            })
-            .send((err) => {
-                if (err) console.log(err)
-            })
+    const handleUpload = async (file) => {
+        uploadFile(file, config)
+            .then(data => console.log(data))
+            .catch(err => console.error(err))
     }
 
-
     return <div>
-        <div>Native SDK File Upload Progress is {progress}%</div>
+        <div>React S3 File Upload</div>
         <input type="file" onChange={handleFileInput}/>
-        <button onClick={() => uploadFile(selectedFile)}> Upload to S3</button>
+        <button onClick={() => handleUpload(selectedFile)}> Upload to S3</button>
     </div>
 }
 
